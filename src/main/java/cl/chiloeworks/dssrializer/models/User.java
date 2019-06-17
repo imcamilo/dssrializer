6122307f54bd53bbdc3c20d761ef5c198a0dfc81
Camilo <camilo.jorquera.a@gmail.com>
2019-06-17 10:35:00 -0400

    feat: create model

---
+++ src/main/java/cl/chiloeworks/dssrializer/models/User.java
@@ -0,0 +1,17 @@
+package cl.chiloeworks.dssrializer.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Camilo Jorquera on 6/17/19
+ */
+@Getter
+@Setter
+public class User {
+
+    private String rut;
+    private String name;
+    private String address;
+
+}
