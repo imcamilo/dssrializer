c8c44cf3b0b8834e8e26a41c27b02e632f7341db
Camilo <camilo.jorquera.a@gmail.com>
2019-06-17 11:00:55 -0400

    feat: add serializer

---
+++ src/main/java/cl/chiloeworks/dssrializer/json/UserJsonSerializer.java
@@ -0,0 +1,26 @@
+package cl.chiloeworks.dssrializer.json;
+
+import cl.chiloeworks.dssrializer.models.User;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+
+/**
+ * Created by Camilo Jorquera on 6/17/19
+ */
+@JsonComponent
+public class UserJsonSerializer extends JsonSerializer<User> {
+
+    @Override
+    public void serialize(User user, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartObject();
+        gen.writeStringField("dni",user.getRut());
+        gen.writeStringField("firstName",user.getName());
+        gen.writeStringField("address",user.getAddress());
+        gen.writeEndObject();
+    }
+
+}
